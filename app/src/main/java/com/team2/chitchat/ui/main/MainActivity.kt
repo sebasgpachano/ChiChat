9712e6c132317d40aedbe60426b800487aa671cb
AntonioJesusRM <antjrm95@gmail.com>
2024-06-06 20:21:29 +0200

    main activity and create login

--- app/src/main/java/com/team2/chitchat/ui/main/MainActivity.kt
+++ app/src/main/java/com/team2/chitchat/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.team2.chitchat
+package com.team2.chitchat.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.team2.chitchat.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
