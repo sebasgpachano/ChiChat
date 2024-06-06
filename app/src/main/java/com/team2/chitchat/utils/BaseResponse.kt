e96b10247bc5fa8442b2f76e8c4a7cbb13f1de74
AntonioJesusRM <antjrm95@gmail.com>
2024-06-06 20:18:31 +0200

    base response

---
+++ app/src/main/java/com/team2/chitchat/utils/BaseResponse.kt
@@ -0,0 +1,7 @@
+package com.team2.chitchat.utils
+
+
+sealed class BaseResponse<T> {
+    //class Success<T>(val data: T) : BaseResponse<T>()
+    //class Error<T>(val error: ErrorModel) : BaseResponse<T>()
+}
