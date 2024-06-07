3549772eed3b99ca89b0125c878b26d26939960b
AntonioJesusRM <antjrm95@gmail.com>
2024-06-07 11:27:00 +0200

    main activity add inheritance to base activity

--- app/src/main/java/com/team2/chitchat/ui/main/MainActivity.kt
+++ app/src/main/java/com/team2/chitchat/ui/main/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.team2.chitchat.ui.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
 import com.team2.chitchat.R
+import com.team2.chitchat.databinding.ActivityMainBinding
+import com.team2.chitchat.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun inflateBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
+
+    override fun observeViewModel() = Unit
+
+    override fun createAfterInflateBindingSetupObserverViewModel(savedInstanceState: Bundle?) {
+        configNavigation()
+    }
+
+    override fun configureToolbarAndConfigScreenSections() = Unit
+
+    private fun configNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
 }
