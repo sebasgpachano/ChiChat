150a371a15999898a060e2dd762a502c083e7185
AntonioJesusRM <antjrm95@gmail.com>
2024-06-07 11:38:39 +0200

    add error shared flow to base view model

--- app/src/main/java/com/team2/chitchat/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/team2/chitchat/ui/base/BaseViewModel.kt
@@ -1,6 +1,7 @@
 package com.team2.chitchat.ui.base
 
 import androidx.lifecycle.ViewModel
+import com.team2.chitchat.data.domain.model.error.ErrorModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 
@@ -8,6 +9,6 @@ abstract class BaseViewModel : ViewModel() {
     protected val loadingMutableSharedFlow = MutableSharedFlow<Boolean>(replay = 0)
     val loadingFlow: SharedFlow<Boolean> = loadingMutableSharedFlow
 
-    /*protected val errorMutableSharedFlow = MutableSharedFlow<ErrorModel>(replay = 0)
-    val errorFlow: SharedFlow<ErrorModel> = errorMutableSharedFlow*/
+    protected val errorMutableSharedFlow = MutableSharedFlow<ErrorModel>(replay = 0)
+    val errorFlow: SharedFlow<ErrorModel> = errorMutableSharedFlow
 }
