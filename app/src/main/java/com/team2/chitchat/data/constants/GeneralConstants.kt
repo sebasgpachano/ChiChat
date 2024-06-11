7a85a98ae432b2290253bfe6515e969d24e6bea6
AntonioJesusRM <antjrm95@gmail.com>
2024-06-11 18:55:18 +0200

    quit api/ to base url

--- app/src/main/java/com/team2/chitchat/data/constants/GeneralConstants.kt
+++ app/src/main/java/com/team2/chitchat/data/constants/GeneralConstants.kt
@@ -4,6 +4,6 @@ class GeneralConstants {
     companion object {
         //Retrofit
         const val RETROFIT_TIMEOUT_IN_SECOND: Long = 30
-        const val BASE_URL: String = "https://mock-movilidad.vass.es/chatvass/api/"
+        const val BASE_URL: String = "https://mock-movilidad.vass.es/chatvass/"
     }
 }
