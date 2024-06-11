fa530b1a509885cf1c0f316cc3c39fdcfc082092
AntonioJesusRM <antjrm95@gmail.com>
2024-06-11 16:18:53 +0200

    change model response to model

--- app/src/main/java/com/team2/chitchat/data/repository/remote/backend/DataProvider.kt
+++ app/src/main/java/com/team2/chitchat/data/repository/remote/backend/DataProvider.kt
@@ -1,14 +1,14 @@
 package com.team2.chitchat.data.repository.remote.backend
 
+import com.team2.chitchat.data.domain.model.users.PostRegisterModel
 import com.team2.chitchat.data.repository.remote.request.users.RegisterUserRequest
 import com.team2.chitchat.data.repository.remote.response.BaseResponse
-import com.team2.chitchat.data.repository.remote.response.users.PostRegisterResponse
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class DataProvider @Inject constructor(private val remoteDataSource: RemoteDataSource) :
     DataSource {
-    override fun postRegisterUser(registerUserRequest: RegisterUserRequest): Flow<BaseResponse<PostRegisterResponse>> {
+    override fun postRegisterUser(registerUserRequest: RegisterUserRequest): Flow<BaseResponse<PostRegisterModel>> {
         return remoteDataSource.postRegisterUser(registerUserRequest)
     }
 }
