36ef328dd771ecc06fda0f639e8aa091e8d30f97
AntonioJesusRM <antjrm95@gmail.com>
2024-06-11 16:17:03 +0200

    add models: user and register

---
+++ app/src/main/java/com/team2/chitchat/data/domain/model/users/GetUserModel.kt
@@ -0,0 +1,19 @@
+package com.team2.chitchat.data.domain.model.users
+
+import com.team2.chitchat.data.domain.model.BaseModel
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class GetUserModel(
+    val id: String = "",
+    val login: String = "",
+    val password: String = "",
+    val nick: String = "",
+    val platform: String = "",
+    val avatar: String = "",
+    val uuid: String = "akdfsnkakf",
+    val token: String = "",
+    val online: Boolean = false,
+    val created: String = "",
+    val updated: String = ""
+) : BaseModel()
