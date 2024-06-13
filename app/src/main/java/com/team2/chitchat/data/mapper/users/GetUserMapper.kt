76b32e51d59194234d563a71da2b756a9c9e74b8
AntonioJesusRM <antjrm95@gmail.com>
2024-06-13 14:02:58 +0200

    add api call list users

--- app/src/main/java/com/team2/chitchat/data/mapper/users/GetUserMapper.kt
+++ app/src/main/java/com/team2/chitchat/data/mapper/users/GetUserMapper.kt
@@ -7,13 +7,9 @@ import com.team2.chitchat.data.repository.remote.response.users.GetUserResponse
 class GetUserMapper : ResponseMapper<GetUserResponse, GetUserModel> {
     override fun fromResponse(response: GetUserResponse): GetUserModel {
         return GetUserModel(
-            response.id ?: "",
             response.login ?: "",
-            response.password ?: "",
             response.nick ?: "",
-            response.platform ?: "",
             response.avatar ?: "",
-            response.uuid ?: "",
             response.token ?: "",
             response.online ?: false,
             response.created ?: "",
--- app/src/main/java/com/team2/chitchat/data/repository/remote/backend/RemoteDataSource.kt
+++ app/src/main/java/com/team2/chitchat/data/repository/remote/backend/RemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.team2.chitchat.data.repository.remote.backend
 
+import com.team2.chitchat.data.domain.model.users.GetUserModel
 import com.team2.chitchat.data.domain.model.users.PostLoginModel
 import com.team2.chitchat.data.domain.model.users.PostRegisterModel
+import com.team2.chitchat.data.mapper.users.GetContactsListMapper
 import com.team2.chitchat.data.mapper.users.PostLoginMapper
 import com.team2.chitchat.data.mapper.users.PostRegisterMapper
 import com.team2.chitchat.data.repository.remote.request.users.LoginUserRequest
@@ -35,4 +37,16 @@ class RemoteDataSource @Inject constructor(private val callApiService: CallApiSe
                 emit(BaseResponse.Error(apiResult.error))
             }
         }
+
+    //ContactsList
+    override fun getContactsList(): Flow<BaseResponse<ArrayList<GetUserModel>>> = flow {
+        val apiResult = callApiService.callGetContactsList()
+        if (apiResult is BaseResponse.Success) {
+            emit(BaseResponse.Success(GetContactsListMapper().fromResponse(apiResult.data)))
+        } else if (apiResult is BaseResponse.Error) {
+            emit(BaseResponse.Error(apiResult.error))
+        }
+    }
+
+
 }
