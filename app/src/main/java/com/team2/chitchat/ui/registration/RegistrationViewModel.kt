749e2316675c9443461a1d5ab4562ff8b2e2a0f2
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-06-17 19:24:52 +0200

    registration viewmodel

---
+++ app/src/main/java/com/team2/chitchat/ui/registration/RegistrationViewModel.kt
@@ -0,0 +1,43 @@
+package com.team2.chitchat.ui.registration
+
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.team2.chitchat.data.repository.remote.request.users.RegisterUserRequest
+import com.team2.chitchat.data.repository.remote.response.BaseResponse
+import com.team2.chitchat.data.usecase.PostRegisterUseCase
+import com.team2.chitchat.ui.base.BaseViewModel
+import com.team2.chitchat.utils.TAG
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RegistrationViewModel @Inject constructor(private val postRegisterUseCase: PostRegisterUseCase) :
+    BaseViewModel() {
+
+    private val successFlow = MutableSharedFlow<Unit>()
+
+    fun postUser(user: String, password: String, nick: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            loadingMutableSharedFlow.emit(true)
+            val newUser = RegisterUserRequest(user, password, nick, "", "and", "", false)
+            postRegisterUseCase(newUser).collect {
+                when (it) {
+                    is BaseResponse.Error -> {
+                        Log.d(this@RegistrationViewModel.TAG, "l> Error: ${it.error.message}")
+                        loadingMutableSharedFlow.emit(false)
+                        errorMutableSharedFlow.emit(it.error)
+                    }
+
+                    is BaseResponse.Success -> {
+                        loadingMutableSharedFlow.emit(false)
+                        Log.d(this@RegistrationViewModel.TAG, "l> Success ${it.data.userModel}")
+                        successFlow.emit(Unit)
+                    }
+                }
+            }
+        }
+    }
+}
