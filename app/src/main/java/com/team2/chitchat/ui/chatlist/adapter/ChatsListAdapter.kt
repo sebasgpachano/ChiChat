3a8c1092ca9483ba33a4681d5d47686abc5cf6ae
AntonioJesusRM <antjrm95@gmail.com>
2024-06-19 16:32:51 +0200

    Add adapter to chat list fragment

---
+++ app/src/main/java/com/team2/chitchat/ui/chatlist/adapter/ChatsListAdapter.kt
@@ -0,0 +1,28 @@
+package com.team2.chitchat.ui.chatlist.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.team2.chitchat.data.domain.model.chats.ListChatsModel
+import com.team2.chitchat.databinding.ItemRecyclerviewListChatsBinding
+
+class ChatsListAdapter(private val listChatsAdapterListener: ListChatsAdapterListener) :
+    ListAdapter<ListChatsModel, ChatsListViewHolder>(ChatsListDiffCallback()) {
+    interface ListChatsAdapterListener {
+        fun onItemClick(idChat: String)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatsListViewHolder {
+        return ChatsListViewHolder(
+            ItemRecyclerviewListChatsBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            ), listChatsAdapterListener
+        )
+    }
+
+    override fun onBindViewHolder(holder: ChatsListViewHolder, position: Int) {
+        holder.onBind(getItem(position), position)
+    }
+}
