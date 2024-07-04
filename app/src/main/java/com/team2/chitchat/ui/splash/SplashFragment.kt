fc6e4a5fb3d5ccc8778574914e7e4270d4b1d452
AntonioJesusRM <antjrm95@gmail.com>
2024-07-04 17:04:21 +0200

    add splash nav

--- app/src/main/java/com/team2/chitchat/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/team2/chitchat/ui/splash/SplashFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.team2.chitchat.R
 import com.team2.chitchat.databinding.FragmentSplashBinding
 import com.team2.chitchat.ui.base.BaseFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : BaseFragment<FragmentSplashBinding>() {
     override fun inflateBinding() {
-        TODO("Not yet implemented")
+        binding = FragmentSplashBinding.inflate(layoutInflater)
     }
 
     override fun createViewAfterInflateBinding(
@@ -17,19 +22,19 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ) {
-        TODO("Not yet implemented")
+        viewLifecycleOwner.lifecycleScope.launch {
+            delay(3000)
+            findNavController().navigate(R.id.action_splashFragment_to_mainNavigation)
+        }
     }
 
     override fun configureToolbarAndConfigScreenSections() {
-        TODO("Not yet implemented")
+        fragmentFullScreenLayoutWithoutToolbar()
     }
 
-    override fun observeViewModel() {
-        TODO("Not yet implemented")
-    }
+    override fun observeViewModel() = Unit
 
-    override fun viewCreatedAfterSetupObserverViewModel(view: View, savedInstanceState: Bundle?) {
-        TODO("Not yet implemented")
-    }
+    override fun viewCreatedAfterSetupObserverViewModel(view: View, savedInstanceState: Bundle?) =
+        Unit
 
 }
