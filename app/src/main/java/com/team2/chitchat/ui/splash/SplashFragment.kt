8763cf56b027ed9f9b61618640c04f0184e88c6a
AntonioJesusRM <antjrm95@gmail.com>
2024-07-04 16:39:30 +0200

    add splash view

---
+++ app/src/main/java/com/team2/chitchat/ui/splash/SplashFragment.kt
@@ -0,0 +1,35 @@
+package com.team2.chitchat.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.team2.chitchat.databinding.FragmentSplashBinding
+import com.team2.chitchat.ui.base.BaseFragment
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>() {
+    override fun inflateBinding() {
+        TODO("Not yet implemented")
+    }
+
+    override fun createViewAfterInflateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun configureToolbarAndConfigScreenSections() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    override fun viewCreatedAfterSetupObserverViewModel(view: View, savedInstanceState: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+}
