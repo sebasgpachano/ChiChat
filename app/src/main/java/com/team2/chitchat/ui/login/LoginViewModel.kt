8a22ca9c9b3caead7c26f9efe7ec1f3071cb6533
Jhostin <jhostinlunadev@gmail.com>
2024-07-22 17:44:16 +0200

    Add load credentials Biometric with Api service

--- app/src/main/java/com/team2/chitchat/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/team2/chitchat/ui/login/LoginViewModel.kt
@@ -3,13 +3,13 @@ package com.team2.chitchat.ui.login
 
 import android.util.Log
 import androidx.lifecycle.viewModelScope
+import com.team2.chitchat.data.domain.model.users.PostLoginModel
 import com.team2.chitchat.data.repository.crypto.BiometricCryptoManager
 import com.team2.chitchat.data.repository.remote.request.users.LoginUserRequest
 import com.team2.chitchat.data.repository.remote.response.BaseResponse
-import com.team2.chitchat.data.usecase.preferences.AccessBiometricUseCase
-import com.team2.chitchat.data.usecase.preferences.GetPasswordLoginUseCase
-import com.team2.chitchat.data.usecase.preferences.PutAccessBiometricUseCase
-import com.team2.chitchat.data.usecase.preferences.SavePasswordLoginUseCase
+import com.team2.chitchat.data.usecase.GetRefreshTokenUseCase
+import com.team2.chitchat.data.usecase.preferences.IsBiometricStateUseCase
+import com.team2.chitchat.data.usecase.preferences.PutBiometricStateUseCase
 import com.team2.chitchat.data.usecase.remote.PostLoginUseCase
 import com.team2.chitchat.ui.base.BaseViewModel
 import com.team2.chitchat.ui.extensions.TAG
@@ -17,6 +17,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.crypto.Cipher
 import javax.inject.Inject
@@ -24,28 +25,24 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val postLoginUseCase: PostLoginUseCase,
-    private val getPasswordLoginUseCase: GetPasswordLoginUseCase,
-    private val savePasswordLoginUseCase: SavePasswordLoginUseCase,
-    private val accessBiometricUseCase: AccessBiometricUseCase,
-    private val putAccessBiometricUseCase: PutAccessBiometricUseCase,
-    private val biometricCryptoManager: BiometricCryptoManager
-) : BaseViewModel() {
-
+    private val getRefreshTokenUseCase: GetRefreshTokenUseCase,
+    private val isBiometricStateUseCase: IsBiometricStateUseCase,
+    private val putBiometricStateUseCase: PutBiometricStateUseCase,
+): BaseViewModel() {
+    // Response of Login
     private val loginMutableStateFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val loginStateFlow: StateFlow<Boolean> = loginMutableStateFlow
-
-    //passwordLogin
-    private val passwordLoginMutableStateFlow: MutableStateFlow<String> = MutableStateFlow("")
-    val passwordLoginStateFlow: StateFlow<String> = passwordLoginMutableStateFlow
-
-    //AccessBiometric
+    
+    //AccessBiometric PREFERENCES
     private val accessBiometricMutableStateFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val accessBiometricStateFlow: StateFlow<Boolean> = accessBiometricMutableStateFlow
 
+    //Get Api RefreshToken
+    private val getRefreshTokenMutableStateFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val getRefreshTokenStateFlow = getRefreshTokenMutableStateFlow.asStateFlow()
     init {
         loadAccessBiometric()
     }
-
     fun doLogin(loginUserRequest: LoginUserRequest) {
         viewModelScope.launch(Dispatchers.IO) {
             loadingMutableSharedFlow.emit(true)
@@ -68,58 +65,44 @@ class LoginViewModel @Inject constructor(
 
     }
 
-    //passwordLogin
-    fun getPasswordLogin(): String {
-        val userPasswordLogin = getPasswordLoginUseCase()
-        return userPasswordLogin
-    }
-
-    fun savePasswordLogin(cipher: Cipher, loginUserRequest: LoginUserRequest) {
+    // RefreshToken
+    fun loaRefreshToken() {
         viewModelScope.launch(Dispatchers.IO) {
-            val encryptPasswordLogin = biometricCryptoManager.encrypt(cipher, loginUserRequest)
-            Log.d(this@LoginViewModel.TAG, "savePasswordLogin: $encryptPasswordLogin")
-            savePasswordLoginUseCase(encryptPasswordLogin)
-        }
-    }
-
-    //AccessBiometric
-    fun loadAccessBiometric() {
-        viewModelScope.launch(Dispatchers.IO) {
-            accessBiometricUseCase().collect { baseResponse ->
-                when (baseResponse) {
+            getRefreshTokenUseCase().collect { baseResponse->
+                when(baseResponse) {
                     is BaseResponse.Error -> {
                         Log.d(this@LoginViewModel.TAG, "l> Error: ${baseResponse.error.message}")
                         errorMutableSharedFlow.emit(baseResponse.error)
                     }
-
                     is BaseResponse.Success -> {
-                        accessBiometricMutableStateFlow.value = baseResponse.data
+                        getRefreshTokenMutableStateFlow.value = baseResponse.data
                     }
                 }
             }
         }
     }
-
+    //AccessBiometric
+    fun loadAccessBiometric() {
+        viewModelScope.launch(Dispatchers.IO) {
+            isBiometricStateUseCase().collect { baseResponse->
+                when(baseResponse) {
+                    is BaseResponse.Error -> {
+                        Log.d(this@LoginViewModel.TAG, "l> Error: ${baseResponse.error.message}")
+                        errorMutableSharedFlow.emit(baseResponse.error)
+                        }
+                    is BaseResponse.Success -> {
+                        accessBiometricMutableStateFlow.value = baseResponse.data
+                    }
+                    }
+                }
+            }
+    }
     fun saveAccessBiometric(accessBiometric: Boolean) {
         viewModelScope.launch(Dispatchers.IO) {
-            putAccessBiometricUseCase(accessBiometric)
+            putBiometricStateUseCase(accessBiometric)
             loadAccessBiometric()
         }
 
-    }
-
-    fun getCipher(isEncrypt: Boolean): Cipher {
-        return if (!isEncrypt) {
-            biometricCryptoManager.decryptCipher(getPasswordLogin())
-        } else {
-            biometricCryptoManager.encryptedCipher()
-        }
-    }
-
-    fun getLogin(cipher: Cipher): LoginUserRequest {
-        val decryptPasswordLogin = biometricCryptoManager.decrypt(cipher, getPasswordLogin())
-        Log.d(this@LoginViewModel.TAG, "decryptPasswordLogin: $decryptPasswordLogin")
-        return decryptPasswordLogin
     }
 
 }
