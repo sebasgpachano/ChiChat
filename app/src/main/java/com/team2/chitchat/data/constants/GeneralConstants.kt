60be624ef0a7b27153fb6b4ec6c1715c340c135d
AntonioJesusRM <antjrm95@gmail.com>
2024-07-22 21:09:37 +0200

    add notifications new message

--- app/src/main/java/com/team2/chitchat/data/constants/GeneralConstants.kt
+++ app/src/main/java/com/team2/chitchat/data/constants/GeneralConstants.kt
@@ -5,7 +5,6 @@ class GeneralConstants {
         //Retrofit
         const val RETROFIT_TIMEOUT_IN_SECOND: Long = 30
         const val BASE_URL: String = "https://mock-movilidad.vass.es/chatvass/"
-
         //Intent keys
         const val INTENT_KEY_PUSH_NOTIFICATION_BODY = "pushNotificationBody"
         const val INTENT_KEY_PUSH_NOTIFICATION_TITLE = "pushNotificationTitle"
